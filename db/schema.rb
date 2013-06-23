0b091c17d41fe741c6f52a3589204a8830519cd3
Shay Frendt <shay.frendt@gmail.com>
2013-06-23 16:15:58 -0400

    Some sort of postgres extension crap

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,8 @@
 
 ActiveRecord::Schema.define(version: 20130623194532) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "courses", force: true do |t|
     t.string "name"
