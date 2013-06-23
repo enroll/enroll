59086aaa20967cc144d91a9e1ef6658cf82d45e6
Shay Frendt <shay.frendt@gmail.com>
2013-06-23 17:47:07 -0400

    Link to the reservation form for a course

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def course_reservation_link(course)
+    link_to 'Reserve your seat',
+      new_course_reservation_path(course),
+      :class => 'btn btn-primary btn-large reserve upcase'
+  end
 end
