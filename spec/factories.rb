66d133d70bc5e34cf122a0befa31fe8ceff9e202
Shay Frendt <shay.frendt@gmail.com>
2013-06-23 19:29:31 -0400

    Add support for paying instructors via Stripe

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,4 +6,19 @@ FactoryGirl.define do
   factory :reservation do
     course
   end
+
+  factory :payout do
+    amount_in_cents 15000 # $150 USD
+    description 'Course: How to raise seed funding'
+    stripe_recipient_id 12345
+
+    factory :payout_requested do
+      status "transfer_requested"
+    end
+
+    factory :payout_completed do
+      status "paid"
+    end
+  end
+
 end
