5da86fe8f5c1166a6f4bc32ae55c0f3eca07266a
Nick Plante <nap@zerosum.org>
2013-06-30 12:57:00 -0400

    add editable_input helper and partial to DRY up view

--- app/models/course.rb
+++ app/models/course.rb
@@ -2,4 +2,7 @@ class Course < ActiveRecord::Base
   has_many :reservations
 
   validates :name, presence: true
+
+  # temporary while we figure out what db columns we want...
+  attr_accessor :tagline, :location, :start_time, :start_date, :description, :motivation, :audience
 end
