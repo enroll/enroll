50a79dd2ccb53c4452d6f306419b58a9af3504ba
Shay Frendt <shay.frendt@gmail.com>
2013-07-28 18:34:07 -0400

    Add a basic edit course form
    - Remove old edit course wysiwyg thing

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -46,6 +46,7 @@ class CoursesController < ApplicationController
   def course_params
     params.require(:course).permit(
       :name, :tagline, :course_starts_at, :course_ends_at, :description,
+      :instructor_biography, :min_seats, :max_seats, :price_per_seat,
       location_attributes: [
         :name, :address, :city, :state, :zip, :phone
       ]
