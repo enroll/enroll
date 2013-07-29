51cd8df2e5c4aed636a34424098735ad880953cd
Shay Frendt <shay.frendt@gmail.com>
2013-07-29 11:04:40 -0700

    Remove postgres schema extension that breaks CI

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
 ActiveRecord::Schema.define(version: 20130728213953) do
 
   create_table "courses", force: true do |t|
