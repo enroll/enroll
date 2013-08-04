80ef9dab7cbf49237af069837c77c3662b427945
Nick Plante <nap@zerosum.org>
2013-08-04 13:10:48 -0400

    course subdomain support, helpers

---
+++ app/helpers/url_helper.rb
@@ -0,0 +1,18 @@
+module UrlHelper
+  def with_subdomain(subdomain)
+    subdomain = (subdomain || "")
+    subdomain += "." unless subdomain.empty?
+    [subdomain, request.domain, request.port_string].join
+  end
+
+  def url_for(options = nil)
+    if options.kind_of?(Hash) && options.has_key?(:subdomain)
+      options[:host] = with_subdomain(options.delete(:subdomain))
+    end
+    super
+  end
+
+  def current_course
+    @course ||= Course.find_by url: request.subdomain
+  end
+end
