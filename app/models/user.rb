8576cb368259c48a091b186f2e6ae9de09d04d38
Shay Frendt <shay.frendt@gmail.com>
2013-08-05 14:05:27 -0400

    Fix up foreign keys on User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :reservations, dependent: :destroy
-  has_many :courses_as_student, through: :reservations, class_name: 'Course', source: :course
-  has_many :courses_as_instructor, class_name: 'Course', foreign_key: :instructor_id, dependent: :destroy
+  has_many :reservations, dependent: :destroy, foreign_key: :student_id
+  has_many :courses_as_student, through: :reservations,
+    class_name: 'Course', source: :course, foreign_key: :student_id
+  has_many :courses_as_instructor, class_name: 'Course',
+    foreign_key: :instructor_id, dependent: :destroy
 
   def courses
     (courses_as_instructor + courses_as_student).uniq
