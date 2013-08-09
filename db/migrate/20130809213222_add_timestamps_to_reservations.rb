e47a5f1da86fed81eb671cf2ea42e922da718ee0
Shay Frendt <shay.frendt@gmail.com>
2013-08-09 17:45:08 -0400

    Use rails for things

--- db/migrate/20130809213222_add_timestamps_to_reservations.rb
+++ db/migrate/20130809213222_add_timestamps_to_reservations.rb
@@ -1,6 +1,5 @@
 class AddTimestampsToReservations < ActiveRecord::Migration
   def change
-    add_column :reservations, :created_at, :datetime
-    add_column :reservations, :updated_at, :datetime
+    add_timestamps(:reservations)
   end
 end
