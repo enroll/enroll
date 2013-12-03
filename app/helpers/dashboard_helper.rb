8b0d039d9e423545a5740081dce6abdf8ade441e
Vojto Rinik <vojto@rinik.net>
2013-12-03 10:06:25 +0100

    Adding button for visiting the landing page from dashboard

--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,12 +1,15 @@
 module DashboardHelper
-  def dashboard_menu_link(path, label, icon, controller, action)
+  def dashboard_menu_link(path, label, icon, controller, action, &block)
     klass = if controller_name == controller && action_name == action
       'active'
     else
       ''
     end
     content_tag :li, :class => klass do
-      content_tag :a, "<i class='icon-#{icon}'></i> #{label}".html_safe, :href => path
+      content_tag :a, :href => path do
+        content = capture_haml(&block) if block_given?
+        content_tag(:i, '', :class => "icon-#{icon}") + ' ' + label + ' ' + content
+      end
     end
   end
 end
