271bc96375eae7a1a1c0a0eeae16717a4651eb0b
Vojtech Rinik <vojto@rinik.net>
2014-01-30 10:13:50 -0500

    Removing comment from devise.rb

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -255,16 +255,5 @@ class CustomFailure < Devise::FailureApp
       redirect
     end
   end
-
-  # def redirect
-  #   store_location!
-  #   if flash[:timedout] && flash[:alert]
-  #     flash.keep(:timedout)
-  #     flash.keep(:alert)
-  #   else
-  #     flash[:alert] = i18n_message
-  #   end
-  #   redirect_to "/"
-  # end
 end
 
