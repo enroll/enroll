23564539dfaa2bb214811c2d89d18efeaef11fdd
Jess Martin and Vojto Rinik <pair+jm+vr@enroll.io>
2014-02-06 14:38:08 -0500

    Moving payments to course form

--- app/controllers/dashboard/payment_settings_controller.rb
+++ app/controllers/dashboard/payment_settings_controller.rb
@@ -1,5 +1,6 @@
-class PaymentSettingsController < ApplicationController
+class Dashboard::PaymentSettingsController < ApplicationController
   before_filter :authenticate_user!
+  before_filter :find_course_as_instructor_by_course_id!, only: [:edit, :update]
 
   def edit
     @user = current_user
@@ -7,9 +8,10 @@ class PaymentSettingsController < ApplicationController
 
   def update
     @user = current_user
+
     if @user.save_payment_settings!(stripe_recipient_params)
       flash[:success] = "Payment settings updated successfully."
-      redirect_to edit_payment_settings_path
+      redirect_to edit_dashboard_course_payment_settings_path(@course.id)
     else
       flash.now[:error] = "Unable to update your payment settings. Sorry about that."
       render :edit
@@ -20,5 +22,11 @@ class PaymentSettingsController < ApplicationController
 
   def stripe_recipient_params
     params.require(:user).permit(:name, :stripe_bank_account_token)
+  end
+
+  def find_course_as_instructor_by_course_id!
+    @course = current_user.courses_as_instructor.find(params[:course_id])
+  rescue ActiveRecord::RecordNotFound
+    redirect_to root_path
   end
 end
