48209b8d4a59bf4cc0355f53a3ec120dafd805cb
Vojtech Rinik <vojto@rinik.net>
2014-02-10 15:50:43 -0500

    Blog

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -14,6 +14,7 @@ class Admin::PostsController < ApplicationController
     @post = BlogPost.new(post_params)
 
     if @post.save
+      @post.publish!
       redirect_to admin_posts_path
     else
       render 'new'
--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -1,3 +1,8 @@
 class BlogPost < ActiveRecord::Base
   acts_as_url :title  
+
+  def publish!
+    self.published_at = Time.now
+    self.save!
+  end
 end
