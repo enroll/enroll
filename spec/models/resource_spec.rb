98beacbc05a25ce45f6a3e88c219a9319208ff0f
Vojtech Rinik <vojto@rinik.net>
2014-02-10 11:29:08 -0500

    Fixing tests

--- spec/models/resource_spec.rb
+++ spec/models/resource_spec.rb
@@ -3,7 +3,6 @@ require 'spec_helper'
 describe Resource do
   it { should validate_presence_of(:course_id) }
   it { should validate_presence_of(:name) }
-  it { should validate_presence_of(:s3_url) }
 
   it "checks type of resource" do
     resource = Resource.new(resource_type: 'file')
