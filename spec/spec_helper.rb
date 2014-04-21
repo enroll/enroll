39721e79dfd6f3f4f157edebc4424aa1ff6fdccf
Jess Martin <jessmartin@gmail.com>
2014-04-21 14:17:31 -0400

    Remove duplicate include.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,6 @@ require 'rspec/rails'
 require 'rspec/instafail'
 require 'vcr'
 require 'email_spec'
-require 'vcr'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
